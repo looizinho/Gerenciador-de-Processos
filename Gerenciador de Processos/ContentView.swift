89568e5fad464ac1e82ea5fef98497a27c14de1b
Luiz Neto <lmoraes@me.com>
2026-05-18 21:18:06 -0300

    Initial Commit

---
+++ Gerenciador de Processos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gerenciador de Processos
+//
+//  Created by Luiz Moraes on 18/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gerenciador de Processos/Gerenciador_de_ProcessosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gerenciador_de_ProcessosApp.swift
+//  Gerenciador de Processos
+//
+//  Created by Luiz Moraes on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Gerenciador_de_ProcessosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
