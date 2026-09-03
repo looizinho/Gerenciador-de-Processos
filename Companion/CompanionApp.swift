87ecb9389d24fe10bbbaa651e670d4ef2d1d30ee
Luiz Neto <lmoraes@me.com>
2026-09-03 07:16:27 -0300

    Initial Commit

---
+++ Companion/CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionApp.swift
+//  Companion
+//
+//  Created by Luizinho on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
